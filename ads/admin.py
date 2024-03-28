d14b6351c4aa05696df7426d5a964e8bba66d65b
hanna <Xzeepchik@gmail.com>
2024-03-29 00:18:22 +0300

    main

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 # Register your models here.
+from .models import Category, Advert
+
+@admin.register(Category)
+class CategoryAdmin(MPTTModelAdmin):
+    list_display = ('id', 'name',)
+    mptt_level_indent = 20
+
+@admin.register(Advert)
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject', 'user', 'category', 'price', 'created', 'moderation')
+    list_display_links = ('subject',)
+    list_filter = ('user', 'category', 'price',)
--- myboard/settings.py
+++ myboard/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SITE_ID = 1
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -122,8 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

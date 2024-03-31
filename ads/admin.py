cea817c65cd4986bcbeab81efc36032593923e9e
hanna <Xzeepchik@gmail.com>
2024-03-31 22:45:08 +0300

    main

--- ads/admin.py
+++ ads/admin.py
@@ -2,16 +2,38 @@ from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 
 # Register your models here.
-from .models import Category, Advert
+from .models import Category, Advert, FilterAdvert
 
 @admin.register(Category)
 class CategoryAdmin(MPTTModelAdmin):
     list_display = ('id', 'name',)
     mptt_level_indent = 20
+    prepopulated_fields = {'slug':('name',)}
 
 
 @admin.register(Advert)
 class AdvertAdmin(admin.ModelAdmin):
-    list_display = ('id', 'subject', 'user', 'category', 'price', 'created', 'moderation')
-    list_display_links = ('subject',)
-    list_filter = ('user', 'category', 'price', )
+    """Объявления"""
+    list_display = (
+        "id",
+        "subject",
+        "user",
+        "category",
+        "filters",
+        "price",
+        "created",
+        "moderation"
+    )
+    list_display_links = ("subject",)
+    list_filter = ("user", "category", "filters", "price")
+    prepopulated_fields = {"slug": ("user", "subject")}
+    search_fields = ("user", "category", "subject", "created")
+
+
+@admin.register(FilterAdvert)
+class FilterAdvertAdmin(admin.ModelAdmin):
+    list_display = ("name", "id")
+    list_display_links = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+
--- ads/models.py
+++ ads/models.py
@@ -15,6 +15,18 @@ class Category(MPTTModel):
         order_insertion_by = ['name']
         verbose_name = "Категория"
 
+class FilterAdvert(models.Model):
+    name = models.CharField("Имя", max_length=50, unique=True)
+    slug = models.SlugField("url", max_length=50, unique=True)
+
+    def __str__(self):
+            return self.name
+
+    class Meta:
+        verbose_name = "Фильтр"
+        verbose_name_plural = "Фильтры"
+
+
 
 class Advert(models.Model):
     """Объявления"""
@@ -28,7 +40,7 @@ class Advert(models.Model):
     created = models.DateTimeField("Дата создания", auto_now_add=True)
     moderation = models.BooleanField("Модерация", default=False)
     slug = models.SlugField("url", max_length=200, unique=True)
-
+    filters = models.ForeignKey(FilterAdvert, verbose_name="Фильтр", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.subject
@@ -37,4 +49,10 @@ class Advert(models.Model):
     class Meta:
         verbose_name = "Объявление"
         verbose_name_plural = "Объявления"
+
+
+
+
+
+
 

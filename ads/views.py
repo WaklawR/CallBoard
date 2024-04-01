54118b19104c86c0a723a3ff26fb18704bbb7dd9
hanna <Xzeepchik@gmail.com>
2024-04-01 16:45:57 +0300

    main

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+from .models import Advert
+
+class AdvertList(ListView):
+    model = Advert
+    queryset = Advert.all()
+    template_name = 'ads/advert_list.html'

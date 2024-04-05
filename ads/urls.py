aef3c7af0e52370ce089fca585342d3a25b7bc81
hanna <Xzeepchik@gmail.com>
2024-04-05 20:55:11 +0300

    main

--- ads/urls.py
+++ ads/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("", AdvertList.as_view(), name="advert-list"),
+    path("<slug:slug>/", AdvertDetail.as_view(), name="advert-info"),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 from .models import Advert
@@ -9,3 +9,9 @@ class AdvertList(ListView):
     template_name = "ads/advert_list.html"
     context_object_name = 'Advert'
     paginate_by = 100
+
+class AdvertDetail(DetailView):
+    """Подробно об объявлении"""
+    model = Advert
+    context_object_name = 'Advert'
+    template_name = "ads/advert_info.html"

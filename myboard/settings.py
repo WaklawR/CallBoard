52e6ae7810b2fc74c6f11e64ea409ad732ad0763
hanna <Xzeepchik@gmail.com>
2024-04-26 12:43:00 +0300

    main

--- myboard/settings.py
+++ myboard/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'image',
     'profiles',
     'comments',
+    'bootstrap4',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -84,10 +86,9 @@ WSGI_APPLICATION = 'myboard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'myboard.data'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
